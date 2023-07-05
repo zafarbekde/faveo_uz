1b82f4768530aace89577e516ac989867deeabd0
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-05 13:18:00 +0500

    ManageUser Done

--- faveo-uz/src/components/app/ManageUser/ManageUser.jsx
+++ faveo-uz/src/components/app/ManageUser/ManageUser.jsx
@@ -9,27 +9,29 @@ function ManageUser() {
           <h1 className="manage-title-name">Registered Users</h1>
         </div>
 
-        <table>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Email</th>
-            <th>Birthday</th>
-            <th>Phone</th>
-            <th>Role</th>
-            <th>Actions</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>jalol</td>
-            <td>Immomadinov</td>
-            <td>some@gmail.com</td>
-            <td>12.12.12</td>
-            <td>+9989456123</td>
-            <td>admin</td>
-          </tr>
-        </table>
+        <div className="manage-wrap-table">
+          <table className='manage-table'>
+            <tr className='manage-table-head'>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Surname</th>
+              <th>Email</th>
+              <th>Birthday</th>
+              <th>Phone</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
+            <tr className='manage-table-item'>
+              <td>1</td>
+              <td>jalol</td>
+              <td>Immomadinov</td>
+              <td>some@gmail.com</td>
+              <td>12.12.12</td>
+              <td>+9989456123</td>
+              <td>admin</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   )
