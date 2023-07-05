d7d9c9a572423b35b7565f19e51571b9e073a9b6
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-05 12:52:13 +0500

    update

--- faveo-uz/src/components/app/ManageUser/ManageUser.jsx
+++ faveo-uz/src/components/app/ManageUser/ManageUser.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import './ManageUser.css'
 
 function ManageUser() {
   return (
-    <div>
-      <h1>sadas</h1>
+    <div className='manage-container'>
+      <div className="manage-wrap">
+        <div className="manage-title">
+          <h1 className="manage-title-name">Registered Users</h1>
+        </div>
+
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Birthday</th>
+            <th>Phone</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>jalol</td>
+            <td>Immomadinov</td>
+            <td>some@gmail.com</td>
+            <td>12.12.12</td>
+            <td>+9989456123</td>
+            <td>admin</td>
+          </tr>
+        </table>
+      </div>
     </div>
   )
 }
