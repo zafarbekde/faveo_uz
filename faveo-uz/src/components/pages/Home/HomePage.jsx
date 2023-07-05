b6f5d14e0b9d9690ee371b36a90f74f248513bfd
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-05 09:43:13 +0500

    add home.css

--- faveo-uz/src/components/pages/Home/HomePage.jsx
+++ faveo-uz/src/components/pages/Home/HomePage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+  import React from 'react'
+  import './home.css'
 function HomePage() {
   return (
-    <div>
-      <h1>sdasf</h1>
+    <div className='home-container'>
+      <div className="home-wrapper">
+        <nav className="home-nav-title">
+          <h1>Faveo</h1>
+        </nav>
+
+        <div className="home-sidebar">
+          <ul className='home-sidebar-item'>
+            <li>Manage Users</li>
+            <li>Categories</li>
+            <li>Products</li>
+            <li>Orders</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
