4efef99e5ab99092750bd2ec617485bfc35557fe
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-05 13:36:14 +0500

    update

--- faveo-uz/src/main.jsx
+++ faveo-uz/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import Login from './components/pages/Login/Login.jsx'
 import HomePage from './components/pages/Home/HomePage.jsx'
 import Register from './components/pages/Register/Register.jsx'
@@ -9,45 +9,49 @@ import ManageUser from './components/app/ManageUser/ManageUser.jsx'
 import Categories from './components/app/Categories/Categories.jsx'
 import Products from './components/app/Products/Products.jsx'
 import Orders from './components/app/Orders/Orders.jsx'
-
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 const rootRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to={"/login"}/> 
+    element: <Navigate to={"/login"} />
   },
   {
     path: "/login",
-    element: <Login/>
+    element: <Login />
   },
   {
     path: "/home",
-    element: <HomePage/>
+    element: <HomePage />
   },
   {
     path: "/register",
-    element: <Register/>
+    element: <Register />
   },
   {
     path: "/manageuser",
-    element: <ManageUser/>
+    element: <ManageUser />
   },
   {
     path: "/categories",
-    element: <Categories/>
+    element: <Categories />
   },
   {
     path: "/products",
-    element: <Products/>
+    element: <Products />
   },
   {
     path: "/orders",
-    element: <Orders/>
+    element: <Orders />
   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <RouterProvider router={rootRouter}>
-    <App />
+    <QueryClientProvider client={QueryClient}>
+      <App />
+    </QueryClientProvider>
   </RouterProvider>
+
 )
