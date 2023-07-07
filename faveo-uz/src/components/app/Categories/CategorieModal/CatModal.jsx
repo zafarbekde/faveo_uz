b6e144bf9bac94a0c274c7db3776006addd50a4e
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-07 13:08:52 +0500

    Update CatModal.jsx

--- faveo-uz/src/components/app/Categories/CategorieModal/CatModal.jsx
+++ faveo-uz/src/components/app/Categories/CategorieModal/CatModal.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function CatModal({close}) {
   return (
-    <div>CatModal
-        <button onClick={() => close()}>Close</button>
+    <div className="categories-modal">
+        <div className="categories-wrap">
+            <div className="categories-head">
+                <h1 className="categories-modal-title"></h1>
+            </div>
+            <div className="categories-button">
+
+            </div>
+        </div>
     </div>
   )
 }
