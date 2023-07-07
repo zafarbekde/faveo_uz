a3cb5fc1fe886e692263448a5b6889d3c4a84c2e
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-07 16:19:11 +0500

    Update

--- faveo-uz/src/components/app/Categories/Categories.jsx
+++ faveo-uz/src/components/app/Categories/Categories.jsx
@@ -9,6 +9,19 @@ function Categories() {
 
   const [showModal, setShowModal] = useState(false);
 
+  const items = [
+    { id: 1, title: 'Food', image: Vector, button: Pen },
+    { id: 2, title: 'Food', image: Vector, button: Pen },
+    { id: 3, title: 'Food', image: Vector, button: Pen },
+    { id: 4, title: 'Food', image: Vector, button: Pen },
+    { id: 5, title: 'Food', image: Vector, button: Pen },
+    { id: 6, title: 'Food', image: Vector, button: Pen },
+    { id: 7, title: 'Food', image: Vector, button: Pen },
+    { id: 8, title: 'Food', image: Vector, button: Pen },
+    { id: 9, title: 'Food', image: Vector, button: Pen },
+    { id: 10, title: 'Food', image: Vector, button: Pen },
+  ]
+
   return (
     <>
       <div className='container-categories'>
@@ -19,14 +32,17 @@ function Categories() {
           </div>
 
           <div className="categories-product">
-            <div className="categories-product-head">
-              <img className='categories-product-img' src={Vector} />
-              <span className="categories-product-title">Foods</span>
-            </div>
+            {items.map((item) => (
+              <div key={item.id} className="categories-product-head">
+                <img className='categories-product-img' src={item.image} />
+                <span className="categories-product-title">{item.title}</span>
+                <div className="categories-button">
+                  <button onClick={() => setShowModal(true)} className='login-btn categories-active'><img src={item.button} /></button>
+                </div>
+              </div>
 
-            <div className="categories-button">
-              <button onClick={() => setShowModal(true)} className='login-btn categories-active'><img src={Pen} /></button>
-            </div>
+
+            ))}
           </div>
         </div>
       </div>
