1c58639866dd279fb46c9389104843796e16388a
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-07 09:50:58 +0500

    Update Categories.jsx

--- faveo-uz/src/components/app/Categories/Categories.jsx
+++ faveo-uz/src/components/app/Categories/Categories.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Categories() {
   return (
-    <div>
-      <h1>asdasd</h1>
+    <div className='container-categories'>
+      <div className="categories-wrap">
+        <h1 className="categories-title">Categories</h1>
+        <button className="login-btn">Create Product</button>
+
+        <div className="categories-product">
+          
+        </div>
+      </div>
     </div>
   )
 }
