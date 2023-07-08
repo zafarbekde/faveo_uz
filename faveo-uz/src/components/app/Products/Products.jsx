07522818d83ede7c98808f3c6ba45e799dac1ba4
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-08 22:12:09 +0500

    update

--- faveo-uz/src/components/app/Products/Products.jsx
+++ faveo-uz/src/components/app/Products/Products.jsx
@@ -3,22 +3,22 @@ import './products.css'
 
 function Products() {
   return (
-    <div className='product-conatiner'>
+    <div className='product-container'>
       <div className="product-wrapper">
         <div className="product-header">
           <div className="product-head">
             <h1 className='product-header-title'>Product</h1>
-            <button className='product-header-button'>Create-Product</button>
+            <button className='login-btn product-header-button'>Create-Product</button>
           </div>
 
           <div className="product-header-filter">
-            <select>
+            <select className='product-filter-categories'> 
               <option value="1">By Categories</option>
               <option value="2">Eng Oxirgi</option>
               <option value="3">Eng Yangi </option>
             </select>
 
-            <select>
+            <select className='product-filter-name'>
               <option value="1">By Name</option>
               <option value="2">By Price</option>
               <option value="3">By Popular</option>
