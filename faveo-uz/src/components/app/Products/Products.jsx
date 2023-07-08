83776fc40a2c9d143585739f2212377b8cc6ba52
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-08 22:08:10 +0500

    Update Product,jsx

--- faveo-uz/src/components/app/Products/Products.jsx
+++ faveo-uz/src/components/app/Products/Products.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './products.css'
 
 function Products() {
   return (
-    <div>
-      <h1>asjjdhsa</h1>
+    <div className='product-conatiner'>
+      <div className="product-wrapper">
+        <div className="product-header">
+          <div className="product-head">
+            <h1 className='product-header-title'>Product</h1>
+            <button className='product-header-button'>Create-Product</button>
+          </div>
+
+          <div className="product-header-filter">
+            <select>
+              <option value="1">By Categories</option>
+              <option value="2">Eng Oxirgi</option>
+              <option value="3">Eng Yangi </option>
+            </select>
+
+            <select>
+              <option value="1">By Name</option>
+              <option value="2">By Price</option>
+              <option value="3">By Popular</option>
+              <option value="4">By Rate</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
