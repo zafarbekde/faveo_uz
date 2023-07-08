f51f40740ed6729bf4ff361ac529ee55be347abe
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-08 21:53:08 +0500

    Upadte

--- faveo-uz/src/components/app/Categories/Categories.jsx
+++ faveo-uz/src/components/app/Categories/Categories.jsx
@@ -28,7 +28,7 @@ function Categories() {
         <div className="categories-wrap">
           <div className="categories-head">
             <h1 className="categories-title">Categories</h1>
-            <button className="login-btn">Create Product</button>
+            <button onClick={() => setShowModal(true)} className="login-btn">Create Product</button>
           </div>
 
           <div className="categories-product">
