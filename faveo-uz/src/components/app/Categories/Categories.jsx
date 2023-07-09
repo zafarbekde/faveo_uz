702fdcd09f0c79cc5766052c43a5a28e8c418c7a
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-07-09 19:43:41 +0500

    update

--- faveo-uz/src/components/app/Categories/Categories.jsx
+++ faveo-uz/src/components/app/Categories/Categories.jsx
@@ -3,14 +3,13 @@ import Pen from '../../assets/pen.png';
 import React, { useState } from 'react';
 import { createPortal } from 'react-dom';
 import CatModal from './CategorieModal/CatModal';
-import Product from '../Products/Products';
 
 function Categories() {
   const [showModal, setShowModal] = useState(false);
-  const [categoryInfo, setCategoryInfo] = useState(null);
+  const [categories, setCategories] = useState([]);
 
   const handleSaveCategory = (info) => {
-    setCategoryInfo(info);
+    setCategories([...categories, info]);
   };
 
   return (
@@ -23,30 +22,22 @@ function Categories() {
           </div>
 
           <div className="categories-product">
-            {categoryInfo && (
-              <div className="categories-product-head">
+            {categories.map((category) => (
+              <div key={category.name} className="categories-product-head">
                 <div className='prosta-div'>
-                  <img className='categories-product-img' src={categoryInfo.selectedImage} alt={categoryInfo.name} />
-                  <span className="categories-product-title">{categoryInfo.name}</span>
+                  <img className='categories-product-img' src={category.selectedImage} alt={category.name} />
+                  <span className="categories-product-title">{category.name}</span>
                 </div>
                 <div className="categories-button">
                   <button onClick={() => setShowModal(true)} className='login-btn categories-active'><img src={Pen} alt="Edit" /></button>
                 </div>
               </div>
-            )}
+            ))}
           </div>
         </div>
       </div>
 
       {showModal && createPortal(<CatModal onSave={handleSaveCategory} close={() => setShowModal(false)} />, document.getElementById("modal-root"))}
-
-      {categoryInfo && (
-        <Product
-          image={categoryInfo.selectedImage}
-          name={categoryInfo.name}
-          price="" // Provide the price for the category
-        />
-      )}
     </>
   );
 }
--- faveo-uz/src/components/app/Products/Products.jsx
+++ faveo-uz/src/components/app/Products/Products.jsx
@@ -2,33 +2,51 @@ import React from 'react';
 import './products.css';
 import Pen from '../../assets/pen.png';
 
-function Product({ product }) {
-  const { selectedImage, name, price } = product || {}; // Assign an empty object as the default value
-
+function Products({ image, name, price }) {
   return (
-    <div className='card'>
-      <div className="card-category">
-        {selectedImage && (
-          <img className='card-category-img' src={selectedImage} alt={name} />
-        )}
-        <span className='card-category-title'>{name}</span>
-      </div>
-      {selectedImage ? (
-        <img className="card-image" src={selectedImage} alt={name} />
-      ) : (
-        <div className="card-image-placeholder">No Image</div>
-      )}
-      <div className="card-details">
-        <div className="product-details">
-          <h2 className="card-name">{name}</h2>
-          <p className="card-price">${price}</p>
+    <div className='product-container'>
+      <div className="product-wrapper">
+        <div className="product-header">
+          <div className="product-head">
+            <h1 className='product-header-title'>Product</h1>
+            <button className='login-btn product-header-button'>Create-Product</button>
+          </div>
+
+          <div className="product-header-filter">
+            <select className='modal-active product-filter-categories'>
+              <option value="1">By Categories</option>
+              <option value="2">Eng Oxirgi</option>
+              <option value="3">Eng Yangi </option>
+            </select>
+
+            <select className='modal-active product-filter-name'>
+              <option value="1">By Name</option>
+              <option value="2">By Price</option>
+              <option value="3">By Popular</option>
+              <option value="4">By Rate</option>
+            </select>
+          </div>
         </div>
-        <button className="card-button">
-          <img className='card-product-img' src={Pen} alt="Edit" /> Edit
-        </button>
+
+        <div className="product-card">
+          <div className="card">
+            <div className="card-category">
+              <img className='card-category-img' src={image} />
+              <span className='card-category-title' >{name}</span>
+            </div>
+            <img className="card-image" src={"image"}  />
+            <div className="card-details">
+              <div className="product-details">
+                <h2 className="card-name">{""}</h2>
+                <p className="card-price">${price}</p>
+              </div>
+              <button className="card-button"><img className='card-product-img' src={Pen} alt="Edit" /> Edit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Product;
+export default Products;
